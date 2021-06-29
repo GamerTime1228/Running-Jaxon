444a254f9b78d28c3085a132bf98ec1f5711e8b8
JAbraham82 <86381298+JAbraham82@users.noreply.github.com>
2021-06-29 17:53:27 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var runnerImg1
+var pathImg
+
+function preload(){
+  //pre-load images
+  pathImg = loadImage("path.png");
+  runnerImg1 = loadAnimation("Runner-1.png", "Runner-2.png");
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  path = createSprite(200,200);
+  path.addImage(pathImg);
+  path.velocityY = 4;
+  path.scale = 1.2;
+
+  runner = createSprite(200,350);
+  runner.addAnimation("runner", runnerImg1);
+  runner.scale = 0.1;
+
+
+
+}
+
+function draw() {
+  background(0);
+  drawSprites();
+
+  if(keyDown("left")) {
+    runner.x = runner.x - 10;
+  }
+
+
+  if(keyDown("right")) {
+    runner.x = runner.x + 10;
+  }
+
+  if(path.y > 400) {
+    path.y = height / 2;
+  }
+
+}
